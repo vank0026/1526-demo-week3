0336b7f5a9ba8ba41805426bb3f6ccb09949be94
roger <roger.van.koughnett@gmail.com>
2012-01-27 11:33:22 -0500

    early form

---
+++ js/jquery.js
@@ -0,0 +1,56 @@
+// JavaScript Document
+
+$(document).ready(function () {
+	// all yquery goes in here, similar to the body tag in html
+	
+	
+/*	all jquery goes through the $() funcion:
+	1. everything we select is done with a css selector
+	2. we do something to that element
+	
+	se can write any scc selector you can thing of in the $()function
+	$("#thing")
+	$(".thing")
+	$("div p")
+	$("div p:first-child")
+	
+*/
+
+	$("a").on("click", function (ev) {
+		
+		//	the preventDefault(); will stof the usual element form doing waht they want, eg. links dont link, forms dont submit
+		
+		ev.preventDefault();
+		console.log("Hello");
+		
+		/* "$(this)" gives us referense to the element this event listener is attached to
+		.attr() lets us get and set element attribuetes
+		*/
+		
+		console.log($(this).attr("href"));
+		
+		// with .css() we get css properties, and the css will be written css-like
+		$(this).css("background-color", "#f33");
+	});
+	
+	$("form").on("submit", function(ev) {
+		ev.preventDefault();
+		
+		var newLi = $(document.createElement("li"));
+		
+		if($("#item").val() != ""){
+			newLi.html($("#item").val());
+			$("ul").append(newLi);
+			$("#item").val("");
+		}
+	});
+
+
+//		**********************
+//		http://api.jquery.com/
+//		**********************
+
+//		the above is the place to go for all jquery syntax stuff
+
+
+});
